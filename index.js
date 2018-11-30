5df4a071c59a8b7f74979dcdbe47f649ea563bd7
Ashwin George Philip <ashwinphilip27@gmail.com>
2018-11-30 02:37:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 
 function moveDodgerRight(){
+<<<<<<< HEAD
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
   
   if(left < 360){
     dodger.style.left = `${left + 1}px`
+=======
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+  
+  if(right > 0){
+    dodger.style.right = `${right - 1}px`
+>>>>>>> 81aad59b7251403b4112f8cc7a4a4a31f4c02a48
   }
 }
 
